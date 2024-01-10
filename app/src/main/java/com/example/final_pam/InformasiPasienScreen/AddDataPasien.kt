39ddd05b5598fde14cc08ed3c7a6fedaa5156283
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 02:04:56 +0700

    make text field id

--- app/src/main/java/com/example/final_pam/InformasiPasienScreen/AddDataPasien.kt
+++ app/src/main/java/com/example/final_pam/InformasiPasienScreen/AddDataPasien.kt
@@ -11,6 +11,8 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -56,7 +58,16 @@ fun AddDataPasien (
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = idPasien,
+                onValueChange = {
+                    idPasien = it
+                },
+                label = {
+                    Text(text = "ID Pasien")
+                }
+            )
         }
     }
 
