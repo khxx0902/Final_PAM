05663c5173318a2c9f799673e0f965c7f80b6742
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 01:57:39 +0700

    add variabel

--- app/src/main/java/com/example/final_pam/InformasiPasienScreen/AddDataPasien.kt
+++ app/src/main/java/com/example/final_pam/InformasiPasienScreen/AddDataPasien.kt
@@ -2,6 +2,10 @@ package com.example.final_pam.InformasiPasienScreen
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -9,5 +13,9 @@ import androidx.navigation.NavController
 fun AddDataPasien (
     navController: NavController,
 ) {
-
+    var idPasien: String by remember { mutableStateOf("") }
+    var nmPasien: String by remember { mutableStateOf("") }
+    var umrPasien: String by remember { mutableStateOf("") }
+    var keluhan: String by remember { mutableStateOf("") }
+    var tglkonsultasi: String by remember { mutableStateOf("") }
 }
