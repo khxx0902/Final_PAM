17d584015282789266bd4f174f735d3897ca87ed
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 01:53:49 +0700

    make function Add data pasien

---
+++ app/src/main/java/com/example/final_pam/InformasiPasienScreen/AddDataPasien.kt
@@ -0,0 +1,13 @@
+package com.example.final_pam.InformasiPasienScreen
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddDataPasien (
+    navController: NavController,
+) {
+
+}
