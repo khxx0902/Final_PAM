b21aa37c17215174cfb7d34a140dad2484b9c8c8
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 11:49:22 +0700

    create function KonsultasiScreen

---
+++ app/src/main/java/com/example/final_pam/KonsultasiPasien/Screen/KonsultasiScreen.kt
@@ -0,0 +1,14 @@
+package com.example.final_pam.KonsultasiPasien.Screen
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.final_pam.KonsultasiPasien.KonsultasiViewModel
+
+@Composable
+fun KonsultasiScreen(
+    navController: NavController,
+    konsultasiViewModel: KonsultasiViewModel = viewModel()
+) {
+
+}
