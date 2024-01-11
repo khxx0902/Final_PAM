4fec2c455ade9b7565f15edb1c35ca38b9436f4e
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 10:45:13 +0700

    add text pasien data

--- app/src/main/java/com/example/final_pam/InformasiPasienScreen/PasienScreen.kt
+++ app/src/main/java/com/example/final_pam/InformasiPasienScreen/PasienScreen.kt
@@ -1,12 +1,42 @@
 package com.example.final_pam.InformasiPasienScreen
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import java.time.format.TextStyle
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun PasienScreen(
     navController: NavController,
     pasienViewModel: PasienViewModel = viewModel()
 ){
+    val allDataState by pasienViewModel.getAllData().collectAsState(initial = emptyList())
 
+    LazyColumn(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        item {
+            Text(
+                text = "Pasien Data",
+                style = androidx.compose.ui.text.TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }
+    }
 }
