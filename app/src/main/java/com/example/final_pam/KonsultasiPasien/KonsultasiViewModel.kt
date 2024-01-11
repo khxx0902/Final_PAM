1abc5613ec1a5cd5dce0afcf0e49395bbcf4ebcf
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 11:35:36 +0700

    make datalist

--- app/src/main/java/com/example/final_pam/KonsultasiPasien/KonsultasiViewModel.kt
+++ app/src/main/java/com/example/final_pam/KonsultasiPasien/KonsultasiViewModel.kt
@@ -1,14 +1,39 @@
 package com.example.final_pam.KonsultasiPasien
 
 import androidx.lifecycle.ViewModel
-import com.example.final_pam.InformasiPasienScreen.DataPasien
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 
 class KonsultasiViewModel() : ViewModel() {
 
-    fun getAllData(): kotlinx.coroutines.flow.Flow<List<DataPasien>> = callbackFlow {
+    fun getAllData(): kotlinx.coroutines.flow.Flow<List<DataKonsultasi>> = callbackFlow {
         val fireStoreRef = Firebase.firestore.collection("konsultasi")
+
+        val subscription = fireStoreRef.addSnapshotListener { value, error ->
+            if (error != null) {
+                // Handle error
+                close(error)
+                return@addSnapshotListener
+            }
+
+            if (value != null) {
+                val dataList = mutableListOf<DataKonsultasi>()
+                for (doc in value.documents) {
+                    val dataKonsultasi = doc.toObject(dataKonsultasi::class.java)
+                    if (dataKonsultasi != null) {
+                        dataList.add(dataKonsultasi)
+                    }
+                }
+                trySend(dataList)
+            }
+        }
+
+        // Cancellation callback
+        awaitClose {
+            subscription.remove()
+        }
+    }
     }
 }
