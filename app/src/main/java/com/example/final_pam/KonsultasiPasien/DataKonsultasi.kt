c6ba2eb17d80e0dc967637ad90f7cdb48cb3fd63
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 11:23:33 +0700

    make dataclass konsultasi

---
+++ app/src/main/java/com/example/final_pam/KonsultasiPasien/DataKonsultasi.kt
@@ -0,0 +1,9 @@
+package com.example.final_pam.KonsultasiPasien
+
+data class DataKonsultasi(
+    var idPasien : String = "",
+    var nmPasien : String = "",
+    var umrPasien : String = "",
+    var keluhan : String = "",
+    var tglkonsultasi : String = "",
+)
