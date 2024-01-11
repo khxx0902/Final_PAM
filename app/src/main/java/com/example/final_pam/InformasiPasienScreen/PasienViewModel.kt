729802c1c64d29aa10521816ba8bb553a43d793b
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 11:10:30 +0700

    create fun saveData

--- app/src/main/java/com/example/final_pam/InformasiPasienScreen/PasienViewModel.kt
+++ app/src/main/java/com/example/final_pam/InformasiPasienScreen/PasienViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.final_pam.InformasiPasienScreen
 
+import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
 
 class PasienViewModel(): ViewModel() {
 
@@ -29,6 +34,23 @@ class PasienViewModel(): ViewModel() {
         }
         awaitClose {
             subscription.remove()
+        }
+    }
+    fun saveData(
+        dataPasien: DataPasien,
+        context: Context
+    )    = CoroutineScope(Dispatchers.IO).launch {
+        val fireStoreRef = Firebase.firestore
+            .collection("pasien")
+            .document(dataPasien.idPasien)
+
+        try{
+            fireStoreRef.set(dataPasien)
+                .addOnSuccessListener {
+                    Toast.makeText(context, "Succsesfully saved data", Toast.LENGTH_LONG).show()
+                }
+        } catch (e: Exception){
+            Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
         }
     }
 }
