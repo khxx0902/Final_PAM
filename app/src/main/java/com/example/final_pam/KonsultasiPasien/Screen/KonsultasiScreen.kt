861767c7c78e73bb9803a5ca6cfa0644263ae77d
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 12:03:13 +0700

    make function list item

--- app/src/main/java/com/example/final_pam/KonsultasiPasien/Screen/KonsultasiScreen.kt
+++ app/src/main/java/com/example/final_pam/KonsultasiPasien/Screen/KonsultasiScreen.kt
@@ -1,11 +1,15 @@
 package com.example.final_pam.KonsultasiPasien.Screen
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
@@ -16,12 +20,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.final_pam.KonsultasiPasien.DataKonsultasi
 import com.example.final_pam.KonsultasiPasien.KonsultasiViewModel
 import com.example.final_pam.Navigasi.Screens
 
@@ -45,7 +51,7 @@ fun KonsultasiScreen(
             )
         }
         items(allDataState) { konsultasiData ->
-            KonsultasiListItem(sewaData = konsultasiData)
+            KonsultasiListItem(konsultasiData = DataKonsultasi())
         }
 
         item {
@@ -63,20 +69,36 @@ fun KonsultasiScreen(
                 }
 
                 OutlinedButton(
-                    onClick = {navController.navigate(route = Screens.AddDataKonsultasiScreen.route) },
+                    onClick = { navController.navigate(route = Screens.AddDataKonsultasiScreen.route) },
                 ) {
                     Text(text = "Add Data Konsultasi")
                 }
 
                 OutlinedButton(
-                    onClick = {navController.navigate(route = Screens.GetDataKonsultasiScreen.route)},
+                    onClick = { navController.navigate(route = Screens.GetDataKonsultasiScreen.route) },
                 ) {
                     Text(text = "Get Data Konsultasi")
                 }
+            }
+        }
     }
 }
 
 @Composable
-fun KonsultasiListItem(sewaData: Int) {
-
+fun KonsultasiListItem(konsultasiData: DataKonsultasi) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(1.dp, Color.Gray)
+            .padding(8.dp)
+    ) {
+        Column {
+            Text(text = "ID Pasien: ${konsultasiData.idPasien}", fontSize = 16.sp)
+            Text(text = "Nama Pasien: ${konsultasiData.nmPasien}")
+            Text(text = "Umur Pasien: ${konsultasiData.umrPasien}")
+            Text(text = "Keluhan: ${konsultasiData.keluhan}")
+            Text(text = "Tanggal Konsultasi: ${konsultasiData.tglkonsultasi}")
+        }
+    }
 }
+
