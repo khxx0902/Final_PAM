98f36875a895490bc2c562daf505ed40101613f5
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 10:06:32 +0700

    make variable

--- app/src/main/java/com/example/final_pam/InformasiPasienScreen/GetDataPasien.kt
+++ app/src/main/java/com/example/final_pam/InformasiPasienScreen/GetDataPasien.kt
@@ -2,6 +2,11 @@ package com.example.final_pam.InformasiPasienScreen
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -10,5 +15,11 @@ fun GetDataPasienScreen(
     navController: NavController,
     pasienViewModel: PasienViewModel,
 ) {
+    var idPasien: String by remember { mutableStateOf("") }
+    var nmPasien: String by remember { mutableStateOf("") }
+    var umrPasien: String by remember { mutableStateOf("") }
+    var keluhan: String by remember { mutableStateOf("") }
+    var tglkonsultasi: String by remember { mutableStateOf("") }
 
+    val context = LocalContext.current
 }
