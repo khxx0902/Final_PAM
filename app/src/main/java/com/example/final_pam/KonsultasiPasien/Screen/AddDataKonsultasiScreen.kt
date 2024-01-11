de535907085d60c21f106d3e3a4e29757e3a03a7
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 12:52:16 +0700

    make main layout and backbutton

--- app/src/main/java/com/example/final_pam/KonsultasiPasien/Screen/AddDataKonsultasiScreen.kt
+++ app/src/main/java/com/example/final_pam/KonsultasiPasien/Screen/AddDataKonsultasiScreen.kt
@@ -1,12 +1,24 @@
 package com.example.final_pam.KonsultasiPasien.Screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.final_pam.KonsultasiPasien.KonsultasiViewModel
 
@@ -23,4 +35,19 @@ fun AddDataKonsultasiScreen (
     var tglkonsultasi: String by remember { mutableStateOf("") }
 
     val context = LocalContext.current
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .padding(start = 15.dp, top = 15.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            IconButton(
+                onClick = { navController.popBackStack() }
+            ) {
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back_button")
+            }
+        }
+    }
 }
