eb653c58ab8fa5130a6bb90d2886261084742252
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 12:49:13 +0700

    add variable

--- app/src/main/java/com/example/final_pam/KonsultasiPasien/Screen/AddDataKonsultasiScreen.kt
+++ app/src/main/java/com/example/final_pam/KonsultasiPasien/Screen/AddDataKonsultasiScreen.kt
@@ -2,6 +2,11 @@ package com.example.final_pam.KonsultasiPasien.Screen
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import com.example.final_pam.KonsultasiPasien.KonsultasiViewModel
 
@@ -11,5 +16,11 @@ fun AddDataKonsultasiScreen (
     navController: NavController,
     konsultasiViewModel: KonsultasiViewModel,
 ) {
+    var idPasien: String by remember { mutableStateOf("") }
+    var nmPasien: String by remember { mutableStateOf("") }
+    var umrPasien: String by remember { mutableStateOf("") }
+    var keluhan: String by remember { mutableStateOf("") }
+    var tglkonsultasi: String by remember { mutableStateOf("") }
 
+    val context = LocalContext.current
 }
