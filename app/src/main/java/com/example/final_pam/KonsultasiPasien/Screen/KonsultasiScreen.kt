6987af09b8e7845375cc6f497be5d57c604d6a1a
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 11:52:33 +0700

    add text

--- app/src/main/java/com/example/final_pam/KonsultasiPasien/Screen/KonsultasiScreen.kt
+++ app/src/main/java/com/example/final_pam/KonsultasiPasien/Screen/KonsultasiScreen.kt
@@ -1,6 +1,17 @@
 package com.example.final_pam.KonsultasiPasien.Screen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.final_pam.KonsultasiPasien.KonsultasiViewModel
@@ -10,5 +21,19 @@ fun KonsultasiScreen(
     navController: NavController,
     konsultasiViewModel: KonsultasiViewModel = viewModel()
 ) {
+    val allDataState by konsultasiViewModel.getAllData().collectAsState(initial = emptyList())
 
+    LazyColumn(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        item {
+            Text(
+                text = "Data Konsultasi",
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+        }
+    }
 }
