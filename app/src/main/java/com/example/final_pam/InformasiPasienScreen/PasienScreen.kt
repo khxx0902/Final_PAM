fb7ea2d3751353d8f2431ab2fc14bfc95afa1ccf
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 10:41:43 +0700

    add function PasienScreen

---
+++ app/src/main/java/com/example/final_pam/InformasiPasienScreen/PasienScreen.kt
@@ -0,0 +1,12 @@
+package com.example.final_pam.InformasiPasienScreen
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun PasienScreen(
+    navController: NavController,
+    pasienViewModel: PasienViewModel = viewModel()
+){
+
+}
