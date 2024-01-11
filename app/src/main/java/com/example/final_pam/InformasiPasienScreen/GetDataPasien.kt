2c3c8e1c4dfa736e52c7ee7e9184737d2c259b0b
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2024-01-11 10:20:06 +0700

    add textfield umur pasien in getdatapasien

--- app/src/main/java/com/example/final_pam/InformasiPasienScreen/GetDataPasien.kt
+++ app/src/main/java/com/example/final_pam/InformasiPasienScreen/GetDataPasien.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -23,6 +24,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -103,7 +105,20 @@ fun GetDataPasienScreen(
                     Text(text = "Nama Pasien")
                 }
             )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = umrPasien,
+                onValueChange = {
+                    umrPasien = it
+                    if (umrPasien.isNotEmpty()) {
+                        umrPasien = umrPasien.toInt().toString()
+                    }
+                },
+                label = {
+                    Text(text = "Umur Pasien")
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
             }
         }
     }
-}
